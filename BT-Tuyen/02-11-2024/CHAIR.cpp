9ae95f527adfc75f0e1e594fda01efae4b6a6d96
SingularDuo <ducphatkieu@gmail.com>
2024-11-04 17:24:49 +0700

    CHAIR.cpp

--- BT-Tuyen/02-11-2024/CHAIR.cpp
+++ BT-Tuyen/02-11-2024/CHAIR.cpp
@@ -7,10 +7,25 @@ int main()
     int n;
     cin>>n;
     vector<int> xich(n);
-    for(int i =0 ; i < n; i++)cin>>vec[i];
-    if(n == 2 || n == 3)
+    for(int i =0 ; i < n; i++)cin>>xich[i];
+    if(n == 2)
     {
-        cout<<1;
+        cout<<"1";
         return 0;
     }
+    else{
+        sort(xich.begin(), xich.end());
+        for(int i = 0; i < n; i++){
+            if(xich[i] == n -2){
+                cout<<xich[i];
+                return 0;
+            }
+        }
+        cout<<n-2+1;
+    }
+
 }
+/**
+ * 3
+ * 3 2 3
+ */
