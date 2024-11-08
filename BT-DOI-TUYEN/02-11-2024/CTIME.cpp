307a88f6f50fbc0cab02f8773ff0b2ad79d76073
quangphat18ti <quangphat18ti@gmail.com>
2024-11-08 22:53:20 +0700

    fix CTIME run to maxx

--- BT-DOI-TUYEN/02-11-2024/CTIME.cpp
+++ BT-DOI-TUYEN/02-11-2024/CTIME.cpp
@@ -6,7 +6,7 @@ const int maxn = 5 * 1e6;
 int main() {
     int n;
     cin >> n;
-    vector<pair<int, int>> a(n);
+    vector< pair<int, int> > a(n);
     for (int i = 0; i < n; i++) cin >> a[i].first >> a[i].second;
     int minn = INT_MAX, maxx = INT_MIN;
     for (int i = 0; i < n; i++) {
@@ -32,7 +32,7 @@ int main() {
     ll max_lam = 0, max_kolam = 0;
     ll current_lam = 0, current_kolam = 0;
 
-    for (int i = minn; i < maxx; i++) {
+    for (int i = minn; i <= maxx; i++) {
         if (luu[i] > 0) {
             current_lam++;
             max_lam = max(max_lam, current_lam - 1);
