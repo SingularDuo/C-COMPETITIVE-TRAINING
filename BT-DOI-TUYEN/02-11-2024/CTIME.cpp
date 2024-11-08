d19f59c6f5ee12b1cc58c5c0a05af370dbe02c39
SingularDuo <ducphatkieu@gmail.com>
2024-11-08 20:20:59 +0700

    New CTIME using Prefix sum

--- BT-DOI-TUYEN/02-11-2024/CTIME.cpp
+++ BT-DOI-TUYEN/02-11-2024/CTIME.cpp
@@ -2,52 +2,48 @@
 using namespace std;
 #define ll long long
 #define endl "\n"
+const int maxn = 5 * 1e6; 
 int main() {
     int n;
     cin >> n;
     vector<pair<int, int>> a(n);
     for (int i = 0; i < n; i++) cin >> a[i].first >> a[i].second;
-    int minn = min_element(a.begin(), a.end(), [](const pair<int, int>& p1, const pair<int, int>& p2) {
-        return p1.first < p2.first;
-    })->first;
-    int maxx = max_element(a.begin(), a.end(), [](const pair<int, int>& p1, const pair<int, int>& p2) {
-        return p1.second < p2.second;
-    })->second;
-    vector<int> luu(maxx + 1, 0);
+    int minn = INT_MAX, maxx = INT_MIN;
     for (int i = 0; i < n; i++) {
-        for (int j = a[i].first; j < a[i].second; j++) {
-            luu[j] = 1;
-        }
+        minn = min(minn, a[i].first);
+        maxx = max(maxx, a[i].second);
     }
+    vector<int> luu(maxx + 2, 0);
+    vector<int> cong(maxx + 2, 0);
+    vector<int> tru(maxx + 2, 0);
+    
+    for (int i = 0; i < n; i++) {
+        int L = a[i].first;
+        int R = a[i].second;
+        cong[L]++;
+        tru[R + 1]++;
+    }
+
+    luu[0] = cong[0] - tru[0];
+    for (int i = 1; i <= maxx; i++) {
+        luu[i] = luu[i - 1] + cong[i] - tru[i];
+    }
+
     ll max_lam = 0, max_kolam = 0;
     ll current_lam = 0, current_kolam = 0;
-    for (int i = minn; i < maxx; i++) {
-        if (luu[i] == 1) {
+
+    for (int i = minn; i <= maxx; i++) {
+        if (luu[i] > 0) {
             current_lam++;
             max_lam = max(max_lam, current_lam);
             current_kolam = 0;
         } else {
             current_kolam++;
             max_kolam = max(max_kolam, current_kolam);
-            current_lam = 0;
+            current_lam = 0; 
         }
     }
-    cout<<max_lam << endl;
-    cout<<max_kolam << endl;
-
+    cout << max_lam << endl;
+    cout << max_kolam<< endl;  
     return 0;
 }
-/*
-3
-100 120
-150 400
-500 1200
-*/
-/*
-5
-3 8
-2 5
-9 12
-19 24
-1 4
-*/
