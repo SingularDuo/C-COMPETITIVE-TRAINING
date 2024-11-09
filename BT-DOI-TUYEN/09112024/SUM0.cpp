a744ec8b7e86d3cf551e30f9131858337be6c2d9
SingularDuo <ducphatkieu@gmail.com>
2024-11-09 09:04:30 +0700

    ADD DIGIT.CPP

--- BT-DOI-TUYEN/09112024/SUM0.cpp
+++ BT-DOI-TUYEN/09112024/SUM0.cpp
@@ -1,21 +1,21 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 #define ll long long
 #define endl "\n"
+
 int main() {
     ll n;
     cin >> n;
-    vector<ll> a(n);
-    unordered_map<ll, ll> cnt;
-    for (ll i = 0; i < n; ++i)cin >> a[i];
-    for (ll i = 0; i < n; ++i) {
-        ll current = -a[i];
-        if (cnt.count(current)!=0) {
-            cout << cnt[current] + 1 <<" "<< i + 1 << endl;
-            return 0;
+    vector<ll> a(n+1);
+    for (ll i = 1; i <= n; ++i) cin >> a[i];  
+    for (ll i = 1; i <= n; i++) {
+        ll target = -1 * a[i];
+        for (ll j = 1; j <= n; j++) {
+            if (a[j] == target) {
+                cout << i << " " << j << endl;  
+                return 0;
+            }
         }
-        cnt[a[i]] = i;
     }
-    cout << "-1 -1" << endl;
     return 0;
 }
