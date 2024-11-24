8e3fd7565dd1c422aa4d05627b93820c773e7546
SingularDuo <ducphatkieu@gmail.com>
2024-11-24 20:47:09 +0700

    OPTIMIZED TUYETCHIEU

--- SELFTRAIN/SelfTrain 22-11-2024/TUYENSINH10_2425_KIEU_DUC_PHAT/TUYETCHIEU.cpp
+++ SELFTRAIN/SelfTrain 22-11-2024/TUYENSINH10_2425_KIEU_DUC_PHAT/TUYETCHIEU.cpp
@@ -1,7 +1,8 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 #define ll long long
 #define endl "\n"
+
 void init() {
     freopen("TUYETCHIEU.inp", "r", stdin);
     freopen("TUYETCHIEU.out", "w", stdout);
@@ -9,21 +10,33 @@ void init() {
     cin.tie(0);
     cout.tie(0);
 }
-int main(){
+
+int main() {
     init();
     ll n, k;
-    cin>>n>>k;
-    vector<int> a(n);
-    for(int i = 1; i <= n;i++)cin>>a[i];
-    for(int i  = 1; i <= n; i++){
-        int nextpos = i+k;
-        for(int j=i+1; j <= nextpos; j++){
-            cout<<a[i]<<" "<<a[j]<<endl;
-            if(a[i] == a[j]){
-                cout<<i;
-                return 0;
+    cin >> n >> k;
+
+    vector<int> a(n + 1); 
+    vector<vector<int>> b(1e5 + 1); 
+
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+        b[a[i]].push_back(i); 
+    }
+
+    for (int i = 1; i <= n; i++) {
+        int val = a[i];
+        for (size_t j = 0; j < b[val].size(); j++) {
+            int pos = b[val][j];
+            if (pos > i && pos < i + k) { 
+                if (a[i] == a[pos]) { 
+                    cout << i << endl;
+                    return 0;
+                }
             }
         }
     }
-    cout<<"-1";
+
+    cout << "-1";
+    return 0;
 }
