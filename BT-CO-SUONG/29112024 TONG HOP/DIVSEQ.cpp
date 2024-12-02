b8ea8f1bfa9dc0b3830a44cd6100a696bccc4b5f
SingularDuo <ducphatkieu@gmail.com>
2024-12-02 18:47:48 +0700

    FIX DIVSEQ

--- BT-CO-SUONG/29112024 TONG HOP/DIVSEQ.cpp
+++ BT-CO-SUONG/29112024 TONG HOP/DIVSEQ.cpp
@@ -13,34 +13,36 @@ void init() {
 
 int main() {
     init();
-    int n, k;
-    cin >> k >> n;
-    vector<int> a(n + 1);
-    for (int i = 1; i <= n; i++) cin >> a[i];
-
-    vector<int> prefix(n + 1);
-    prefix[0] = 0;
-    map<int, int> cnt;
-    for (int i = 1; i <= n; i++) {
-        prefix[i] = prefix[i - 1] + a[i];
-        cnt[prefix[i] % k]++;
-    }
-    int soluong = 0;
-    for (auto i : cnt) {
-        if (i.second > 1) {
-            soluong += (i.second * (i.second - 1)) / 2; 
+    ll t;
+    cin >> t;
+    while (t--) {
+        ll n, k;
+        cin >> k >> n;
+        vector<ll> a(n + 1);
+        for (ll i = 1; i <= n; i++) cin >> a[i];
+        vector<ll> prefix(n + 1);
+        prefix[0] = 0;
+        map<ll, ll> cnt;
+        for (ll i = 1; i <= n; i++) {
+            prefix[i] = prefix[i - 1] + a[i];
+            cnt[prefix[i] % k]++;
         }
+        cnt[0] ++;
+        ll soluong = 0;
+        for (auto i : cnt) {
+            if (i.second > 1) {
+                soluong += (i.second * (i.second - 1)) / 2;
+            }
+        }
+        
+        cout << soluong << endl;
     }
-    cout << soluong;
-
     return 0;
 }
 
 /*
-logic theo bài SUMSEQ0, sẽ có mối liên hệ giữa các phần tử %k   == 0
-*/
-/*
-cần tìm mối liên hệ giữa các đoạn, prefix sum %k == 0
+Logic theo bài SUMSEQ0, sẽ có mối liên hệ giữa các phần tử %k == 0
+Cần tìm mối liên hệ giữa các đoạn, prefix sum %k == 0
 */
 /*
 6 5
