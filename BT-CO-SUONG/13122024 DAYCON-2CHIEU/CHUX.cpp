b7fe9d5e0baced99a98b0e63d355a1e504bf825c
SingularDuo <ducphatkieu@gmail.com>
2024-12-13 19:10:37 +0700

    ADD LOTS OF FILE FROM CO SUONG

---
+++ BT-CO-SUONG/13122024 DAYCON-2CHIEU/CHUX.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl "\n"
+ll a[1005][1005];
+ll f[1005][1005];
+void init(){
+    freopen("CHUX.INP", "r", stdin);
+    freopen("CHUX.OUT", "w", stdout);
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+}
+int main(){
+    init();
+    ll n, k;
+    cin >> n >> k;
+    for(ll i = 1; i <= n; i++){
+        for(ll j = 1; j <= n; j++){
+            cin >> a[i][j];
+        }
+    }
+    ll maxx = INT_MIN;
+    for(ll i = k - 1; i <= n - k + 2; i++){
+        for(ll j = k - 1; j <= n - k + 2; j++){
+            ll currval = a[i][j];
+            for(ll l = 1; l <= k / 2; l++){
+                currval += a[i-l][j-l];
+                currval += a[i+l][j+l];
+                currval += a[i-l][j+l];
+                currval += a[i+l][j-l];
+            }
+            maxx = max(maxx, currval);
+        }
+    }
+    cout << maxx;
+}
