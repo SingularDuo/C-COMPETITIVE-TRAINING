fd07e8a8ce02d7f1d5da92e77bc4775e2672fdde
SingularDuo <ducphatkieu@gmail.com>
2025-01-24 19:22:00 +0700

    fix

--- BT-CO-SUONG/DP_B/DP_B7.cpp
+++ BT-CO-SUONG/DP_B/DP_B7.cpp
@@ -4,11 +4,11 @@ using namespace std;
 #define endl "\n"
 int main(){
     ll n;
-    cin>>n;
+    cin >> n;
     ll s1 = 1, cnt = 0;
-    while(n >= s1){
+    while (n >= s1) {
         cnt++;
         s1++;
     }
-    cout<<cnt;
+    cout << cnt;
 }
