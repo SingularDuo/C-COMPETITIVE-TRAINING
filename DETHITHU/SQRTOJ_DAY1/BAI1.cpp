bf5da0dfda8c7567e7059190447fd349869498af
SingularDuo <ducphatkieu@gmail.com>
2025-03-08 22:13:24 +0700

    ADD WOOD-70% AND WTRANS 42%

--- DETHITHU/SQRTOJ_DAY1/BAI1.cpp
+++ DETHITHU/SQRTOJ_DAY1/BAI1.cpp
@@ -1,56 +1,54 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 #define int long long
 #define ll long long
-#define KING_PHAT signed main()
 #define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define endl '\n'
+
 const int MOD = 1e9 + 7;
-void init()
-{
-    freopen("WOOD.INP", "r", stdin);
-    freopen("WOOD.OUT", "w", stdout);
-    fast;
-}
+
 int a, b, n, m;
 set<int> sums;
 
-void gen(ll sum, ll curra, ll currb) {
-    if(sum != 0) sums.insert(sum);;
-    if (curra > 0) gen(sum + a, curra - 1, currb);
-    if (currb > 0) gen(sum + b, curra, currb - 1);
-}
-KING_PHAT {
-    init();
+signed main() {
     fast;
-    cin>>a>>b>>n>>m;
-    if(n == 1 && m == 1)
-    {
-        if(a == b){
-            cout<<2;
-            return 0;
-        }
-        cout<<"3";
-        return 0;
-    }
-    else if(a == b)
-    {
-        int max_value = a*(n+m);
-        cout<<(n+m) % MOD;
-        return 0;
-    }
-    else if( m == 1){
-        cout<<n * 2;
-        return 0;
+    freopen("WOOD.INP", "r", stdin);
+    freopen("WOOD.OUT", "w", stdout);
 
-    }
-    else if( n <= 10 && m <= 10)
-    {
-        gen(0,n,m);
-        cout<<sums.size() % MOD<<endl;
+    cin >> a >> b >> n >> m;
+
+    if (n == 1 && m == 1) {
+        if (a == b) {
+            cout << 2;
+        } else {
+            cout << 3;
+        }
         return 0;
     }
-    gen(0, n, m);
-    cout << sums.size() % MOD << endl;
+
+    if (a == b) {
+        cout << (n + m) % MOD;
+        return 0;
+    }
+
+    if (m == 1) {
+        if (b % a == 0) {
+            cout << n * 2;
+        } else {
+            cout << n * 2 + 1;
+        }
+        return 0;
+    }
+
+    set<int> cnt;
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <= m; j++) {
+            int current = i * a + j * b;
+            cnt.insert(current);
+        }
+    }
+
+    cout << (cnt.size() - 1) % MOD;
     return 0;
 }
--- DETHITHU/SQRTOJ_DAY1/BAI2.cpp
+++ DETHITHU/SQRTOJ_DAY1/BAI2.cpp
@@ -1,53 +1,41 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-#define ll long long
 #define int long long
-const int maxn = 1e6;
-const int dr[4] = {0, -1, 0, 1};
-const int dc[4] = {-1, 0, 1, 0};
-int n, m;
-bool ValidCell(int r, int c) {
-    return (r >= 0 && r < n && c >= 0 && c < m);
-}
-ll calculate(ll i, ll j, const vector<vector<ll>>& a){
-    ll res = a[i][j];
-    ll val1=0,val2=0,val3=0,val4=0;
-    for(int i = 0; i < n; i++){
-        ll nx = i + dr[i], ny = j + dc[i];
-        if(ValidCell(nx, ny)){
-            res += a[nx][ny];
-        }
-    }
-    return res;
-}
+#define ll long long
+#define KING_PHAT signed main()
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl '\n'
 
-signed main()
-{
+KING_PHAT
+{`
+    //freopen("WTRANS.INP", "r", stdin);
+    //freopen("WTRANS.OUT", "w", stdout);
+    fast;
 
-
-    cin>>n>>m;
+    ll n, m;
+    cin >> n >> m;
+    vector<pair<ll, ll>> wood;
     vector<vector<ll>> a(n, vector<ll>(m));
-    for(int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
-        for(int j = 0; j < n; j++)
+        for (int j = 0; j < m; j++)
         {
-            cin>>a[i][j];
+            cin >> a[i][j];
+            if (a[i][j] > 0)  
+                wood.push_back({i, j}); 
         }
     }
-    ll pos1 = 0, pos2 = 0, maxx = LLONG_MIN;
-    for(int i = 0; i < n; i++)
+    ll minn = LLONG_MAX;
+    for (auto [i, j] : wood) 
     {
-        for(int j = 0; j < m; j++){
-            ll curr = calculate(i,j,a);
-            if(curr > maxx)
-            {
-                pos1 = i;
-                pos2 = j;
-                maxx = curr;
-            }
+        ll curr = 0;
+        for (auto [x, y] : wood)
+        {
+            ll calc = abs(i - x) + abs(j - y);
+            curr += calc * a[x][y];
         }
+        minn = min(minn, curr);
     }
-    cout<<pos1<<pos2;
 
-
+    cout << minn << endl;
 }
