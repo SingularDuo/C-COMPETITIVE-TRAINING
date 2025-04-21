3d6adee392a85ff28b40cf81e31506d6b976079d
SingularDuo <ducphatkieu@gmail.com>
2025-04-21 19:41:50 +0700

    new problem solved

--- ROADMAP MARISA/LVL2/BINSEARCH/Consecutive_integers.cpp
+++ ROADMAP MARISA/LVL2/BINSEARCH/Consecutive_integers.cpp
@@ -1,39 +1,51 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 #define int long long
 #define ll long long
 #define KING_PHAT signed main()
 #define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define endl '\n'
 
-KING_PHAT
-{
-    fast;
-    int n;
-    cin>>n;
-    vector<ll> a(n);
-    int minn = LLONG_MAX;
-    int max = maxxxx();
-    for(int i =0; i < n; i++)
-    {
-        cin>>a[i];
-        minn = min(minn, a[i]);
-    }
-    sort(a.begin(), a.end());
-    vector<ll> sorted_A(n);
-    for(int i = 0; i < n; i++)
-    {
-        sorted_A[i] = minn;
-        minn++;
-    }
-    int res = 0;
-    for(int i = 0; i < n; i++)
-    {
-        if(a[i] != sorted_A[i])res++;
-    }
-    cout<<res;
+int n, start, maximum;
+vector<ll> a;
+vector<int> getUniqueElements(const vector<int>& inputVector) {
+    set<int> uniqueSet(inputVector.begin(), inputVector.end());
+    return vector<int>(uniqueSet.begin(), uniqueSet.end());
 }
-// count()
-// 6 6 7 7 8 9 10 11 11
-// 6 7 8 9 10 11 12 13 14 day kq
-// 
+vector<ll> genans(int start, int range) {
+    vector<ll> ans;
+    for (int i = start; i < start + range; i++) {
+        ans.push_back(i);
+    }
+    return ans;
+}
+KING_PHAT {
+    fast;
+    cin >> n;
+    int subn = n;
+    a.resize(n);
+    map<int, int> cnt;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+        cnt[a[i]]++;
+    }
+    a = getUniqueElements(a);
+    sort(a.begin(), a.end());
+
+    ll res = LLONG_MAX;
+
+    for (int i = 0; i < a.size(); i++) {
+        ll currmax = a[i] + subn-1;
+        ll appear = 0;
+        for(int j = i; j < a.size();j++)
+        {
+            if(a[j] > currmax)break;
+            //cerr<<a[j]<<" "<<currmax<<endl;
+            appear++;
+        }
+        res  = min(res, subn - appear);
+    }
+
+    cout << res;
+}
