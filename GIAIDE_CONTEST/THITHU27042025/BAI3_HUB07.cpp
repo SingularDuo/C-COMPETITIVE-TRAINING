8f6989f404be9e5fb51695dc46e2a450ffcd35eb
SingularDuo <ducphatkieu@gmail.com>
2025-05-03 22:26:32 +0700

    add solution thi thu PTNK

--- GIAIDE_CONTEST/THITHU27042025/BAI3_HUB07.cpp
+++ GIAIDE_CONTEST/THITHU27042025/BAI3_HUB07.cpp
@@ -11,46 +11,63 @@ using namespace std;
 #define I_O(input_name, output_name) freopen((string(input_name) + ".inp").c_str(), "r", stdin); freopen((string(output_name) + ".out").c_str(), "w", stdout); fast;
 
 bool multitest = false;
+
 string tru(const string& a, const string& b) {
-    string result = "";
-    int borrow = 0, diff = 0;
-    int n1 = a.length(), n2 = b.length();
+    string result = ""; // o(1)
+    int borrow = 0, diff = 0; // o(1)
+    int n1 = a.size(), n2 = b.size(); //o(1)
 
-    for (int i = 0; i < n1; i++) {
-        int digit1 = a[n1 - 1 - i] - '0';
-        int digit2 = (i < n2) ? b[n2 - 1 - i] - '0' : 0;
-
-        diff = digit1 - digit2 - borrow;
-        if (diff < 0) {
+    for (int i = 0; i < n1; i++) { // o(n)
+        int digit1 = a[n1 - 1 - i] - '0'; // o(1)
+        int digit2;
+        if(i < n2)
+        {
+            digit2 = b[n2 - 1 - i] - '0'; // o(1)
+        }
+        else digit2 = 0;
+        diff = digit1 - digit2 - borrow; // o(1)
+        if (diff < 0) { 
             diff += 10;
             borrow = 1;
         } else {
-            borrow = 0;
+            borrow = 0; 
         }
-        result = char(diff + '0') + result;
+        result += char(diff + '0');
+// 20 + 5 ==> 20 + 5 = 205
+// result = char(diff + '0) + result; --
+//                                 |-> thực hiện *phép nối string* => tốc độ thực thi là result.size() => **TLE**
+// result = result + char(diff + '0');--
+//20 + 5 => 20 + 5 => 205
     }
-    while (result.length() > 1 && result[0] == '0') {
-        result.erase(0, 1);
-    }
-
+    ll idx = 0;
+    reverse(result.begin(), result.end());
+    while (result.length() > 1 && result[idx] == '0') {
+        idx++;
+    } /** */
+    result.erase(0, idx);
+    
     return result;
 }
 void sol() {
     string s;
     int k;
     cin >> s >> k;
+    // n = s.size()
     string sub = s;
 
     string result = "";
     int keep = s.size() - k;
-    
+
     for (char c : s) {
         while (!result.empty() && result.back() < c && k > 0) {
-            result.pop_back();
+            result. pop_back();
             k--;
         }
+        // 38492498293487987234
+        // 39492498293487987234
         result.push_back(c);
     }
+    // luu string co gia tri lon nhat sau khi xoa k chu so
     result.resize(keep);
     cout<<tru(sub, result);
 }
@@ -68,4 +85,5 @@ KING_PHAT {
     }
     return 0;
 }
+
 // 8380248390522737902399703986576270399917236558147138480460326941378838153543659549175541883522667841718787744915558298207485830622522715211018760095628594390563630840464643840093757297565304855542192458625139737502187155856794161204459753905563268914887193114959093407575064784006247418589993377325634060405685376867184973102371893748040570056780657343048759532739779533385896042337001030038360409249291093003019869016448614081031150849305238780457106033502327100971745178254530194593966408206072981161181150083
--- GIAIDE_CONTEST/THITHU27042025/BAI4_HUB08.cpp
+++ GIAIDE_CONTEST/THITHU27042025/BAI4_HUB08.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 #define ll long long
 #define endl "\n"
+#define min(a, b) min((ll)a, (ll)b)
 
 void init() {
     freopen("BAI4_HUB08.inp", "r", stdin);
@@ -11,49 +12,74 @@ void init() {
     cout.tie(0);
 }
 
+ll my_pow(ll n, ll k) {
+    if (k == 0) return 1;
+    if (k == 1) return n;
+
+    ll res = my_pow(n, k / 2);
+    res *= res;
+    if (k % 2) res *= n;
+
+    return res;
+}
+
 signed main() {
-    init();
+    //init();
 
     ll n, k, s = 0;
     cin >> n;
-    vector<ll> a(n + 1), f(n + 1, 0);
+    vector<ll> a(n + 1), f(n + 1, 0), f_min(n + 1, LLONG_MAX);
 
     for (ll i = 1; i <= n; i++) {
         cin >> a[i];
         s += a[i];
         f[i] = f[i - 1] + a[i];
+        f_min[i] = min(f_min[i - 1], a[i]);
     }
 
-    k = (s+1)/2;
+    ll limit = floor(log2(n)) + 1;
+    vector<vector<ll>> dp_f(n+1, vector<ll>(limit));
+
+    // f[i][0] = a[i]
+    for (ll i = 1; i <= n; i++) dp_f[i][0] = a[i];
+
+    for (ll k = 1; k < limit; k++) {
+        for (ll i = 1; i + int(pow(2, k))-1 <= n; i++) {
+            dp_f[i][k] = min(dp_f[i][k - 1], dp_f[i + int(pow(2, k - 1))][k-1]);
+        }
+    }
+
+    k = (s + 1) / 2;
     ll res = LLONG_MAX;
     ll l = 1;
+
     for (ll r = 1; r <= n; r++) {
-        
-        //cout<<"r: "<<r<<endl;
-        while (f[r] - f[l] > k && l <= r) 
-        {
-            //cout<<l<<" "<<r<<endl;
+        // cout<<"r: "<<r<<endl;
+        while (f[r] - f[l] > k && l <= r) {
+            // f[l+1] -> f[r]
+            // cout<<l<<" "<<r<<endl;
             l++;
         }
-        //while(f[r] - f[l-1] < k)l--;
-        //cout<<"done loop"<<endl; 
-        //cout<<l<<" "<<r<<endl;
+        // while(f[r] - f[l-1] < k)l--;
+        // cout<<"done loop"<<endl; 
+        // cout<<l<<" "<<r<<endl;
         if (f[r] - f[l - 1] < k) continue;
-        //cout<<"valid "<<l<<" "<<r<<endl;
+        // cout<<"valid "<<l<<" "<<r<<endl;
 
         ll v = f[r] - f[l - 1];
-        //cout<<v<<endl;
+        // cout<<v<<endl;
         if (v == k) {
-            //cout<<"v = k"<<endl;
+            // cout<<"v = k"<<endl;
             res = min(res, r - l + 1);
         } else {
-            //cout<<"get "<<l<<" "<<r<<endl;
-            //cout<<l<<" "<<r<<endl;
-            
+            // cout<<"get "<<l<<" "<<r<<endl;
+            // cout<<l<<" "<<r<<endl;
             ll du = v - k;
-            ll minn = LLONG_MAX;
-            for(ll i  = l; i <= r; i++)minn = min(minn, a[i]);
-            if(v - minn >= k)res = min(res, r - l);
+            ll log_range = floor(log2(r - l + 1));
+            ll minn = min(dp_f[l][log_range], dp_f[r - int(pow(2, log_range)) + 1 ][log_range]);
+
+            // min[i][k] = min i --> i + 2^k-1
+            if (v - minn >= k) res = min(res, r - l);
             else res = min(res, r - l + 1);
         }
     }
